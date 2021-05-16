d751da0b6af3e91bf7f2ba6598733dc76048f9a0
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-05-16 17:59:51 +0200

    Modify test Action

--- tests/test_finance.py
+++ tests/test_finance.py
@@ -2,12 +2,7 @@ from finance import Action
 
 
 class TestAction:
-    action = Action("share1", 50, 0.05)
-    def test_name(self):
-        assert self.action.name == "share1"
+    action = Action("share1", 50, 5)
 
-    def test_price(self):
-        assert self.action.price == 50
-
-    def test_profit(self):
-        assert self.action.profit == 0.05
+    def test_net_profit(self):
+        assert self.action.net_profit == 50 * 5 / 100
