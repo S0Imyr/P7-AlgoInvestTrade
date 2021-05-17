f5c99c21e74ea74cabebe04df1b6bb9c17c61292
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-05-17 16:58:14 +0200

    Fix a problem with portfolio price

--- finance.py
+++ finance.py
@@ -19,21 +19,26 @@ class Portfolio:
     """Create a protfolio object with multiple actions"""
     def __init__(self):
         self.actions = []
+        self.price = 0
+        self.profit = 0
 
     def __repr__(self):
         display = display_cell_length('Action', 12) + ' | ' \
                   + display_cell_length('Price', 12) + ' | ' \
                   + display_cell_length('Profit', 12) + '\n'
         for action in self.actions:
-            display += display_cell_length(action.id, 12) + ' | ' \
+            display += display_cell_length(action.name, 12) + ' | ' \
                        + display_cell_length(action.price, 12) + ' | ' \
-                       + display_cell_length(action.profit, 12) + '\n'
+                       + display_cell_length(str(action.profit) + '%', 12) + '\n'
         return display
 
-    def add_actions(self, names, prices, profits):
+    def add_data_actions(self, names, prices, profits):
         """Check if the data can be converted into actions and then add them to the portfolio"""
         if len(names) != len(prices) or len(names) != len(profits) or len(prices) != len(profits):
             raise ValueError("The prices data and the profits data must coincide. The number of element don't match")
         else:
             for action in range(len(names)):
                 self.actions.append(Action(names[action], prices[action], profits[action]))
+                self.price += prices[action]
+                self.profit += prices[action] * profits[action]
+
