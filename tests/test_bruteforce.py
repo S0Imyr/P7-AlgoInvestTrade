dd3497ea6bd05d4e2376aa6df2e050112ffcd790
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-07-07 00:41:34 +0200

    modify test dynamic programming returning lists

--- tests/test_bruteforce.py
+++ tests/test_bruteforce.py
@@ -8,10 +8,3 @@ def test_best_portfolios():
 
 def list_porfolios():
     pass
-
-
-
-"""    names, prices, profits = ["share1", "share2", "share3"], [15, 45, 20], [5, 7, 10]
-    market1 = Portfolio()
-    market1.add_data_actions(names, prices, profits)
-    print(best_portfolios(list_portfolios(market1.actions), 60))"""
--- tests/test_optimised.py
+++
@@ -1,8 +0,0 @@
-from optimised import dynamic_programming
-
-
-def test_dynamic_programming():
-    assert dynamic_programming(7, [5, 3, 1, 4], [100, 55, 18, 70]) == (125.0, 'Action 2 - Action 4 - ')
-    assert dynamic_programming(15, [2, 5, 7, 12, 9], [1, 2, 3, 7, 10]) == (12.0, 'Action 2 - Action 5 - ')
-    assert dynamic_programming(12, [1, 2, 5, 6, 7], [1, 6, 18, 22, 24]) == (42.0, 'Action 3 - Action 5 - ')
-    assert dynamic_programming(10, [2, 3, 4, 4, 8], [3, 5, 6, 7, 10]) == (16.0, 'Action 1 - Action 3 - Action 4 - ')
