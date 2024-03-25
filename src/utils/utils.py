e56cff7dfabef4ce5a7e0baef2d9fe07e17e9348
rlossec <raphaelossec@gmail.com>
2024-03-26 00:08:38 +0100

    Clean utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,8 +1,6 @@
 
-from operator import attrgetter
 
-
-def display_cell_length(message, length):
+def display_cell_length(message: str, length: int):
     if len(str(message)) > length:
         return str(message)[:length]
     else:
@@ -19,23 +17,3 @@ def input_positive_integer(message: str) -> int:
                 return number
         except ValueError:
             print("Veuillez entrer un nombre entier.")
-
-
-# def display_sorted_results(results):
-#     sorted_results = sorted(results, key=attrgetter("price", "profit_amount"), reverse=True)
-#     display = (display_cell_length('Actions achetées', 50) + ' | '
-#                + display_cell_length('Price', 12) + ' | '
-#                + display_cell_length('Profit', 12) + '\n')
-#     for node in sorted_results[:100]:
-#         composition = ""
-#         for share, num in node.history.items():
-#             if num != 0:
-#                 composition += f"{num} {share} - "
-#         display += (display_cell_length(composition[:-2], 50) + ' | '
-#                     + display_cell_length(node.price, 12) + ' | '
-#                     + display_cell_length(node.profit_amount, 12) + '\n')
-#     print(display)
-
-
-if __name__ == '__main__':
-    pass
