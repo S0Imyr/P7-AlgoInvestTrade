8401ce3e7086465f42672db83dd61b62d57883fa
rlossec <raphaelossec@gmail.com>
2024-03-25 23:13:04 +0100

    Add typing in Share model

--- src/models/share.py
+++ src/models/share.py
@@ -1,23 +1,33 @@
 
 class Share:
-    """Share model"""
-    def __init__(self, name, price, profit_percentage):
-        if price < 0 or profit_percentage < 0:
-            raise ValueError("Price and profit percentage must be non-negative.")
+    """Share model representing a stock or share."""
+    def __init__(self, name: str, price: float, profit_percentage: float):
+        """
+        Initialize a Share instance.
+
+        Args:
+            name: The name or identifier of the share.
+            price The price of the share.
+            profit_percentage: The profit percentage of the share.
+
+        Raises:
+            ValueError: If price or profit_percentage is not positive.
+        """
+        if price <= 0 or profit_percentage <= 0:
+            raise ValueError("Price and profit percentage must be positive.")
         self.name = name
         self.price = price
         self.profit_percentage = profit_percentage
 
     @property
-    def profit_amount(self):
-        """Calculate and return the net profit."""
+    def profit_amount(self) -> float:
+        """Calculate and return the amount of profit of the share."""
         return self.price * self.profit_percentage / 100
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Action {self.name}: \n' \
                f'Price: {self.price} \n' \
                f'Profit: {self.profit_percentage}%'
-
 
 
 if __name__ == '__main__':
@@ -32,11 +42,22 @@ if __name__ == '__main__':
     share1 = Share("ABC", 10, 5)
     print("Montant", share1.profit_amount, 0.5)
 
-        # Test with zero price and profit
-    share2 = Share("XYZ", 0, 0)
-    print("Montant", share2.profit_amount, 0)
+    # Tests avec des valeurs négatives
+    try:
+        share3 = Share("DEF", 20, -2)
+    except ValueError as e:
+        print(e)
+    try:
+        share4 = Share("GHI", -20, 2)
+    except ValueError as e:
+        print(e)
+    # Tests avec des valeurs nulles
+    try:
+        share3 = Share("DEF", 20, 0)
+    except ValueError as e:
+        print(e)
 
-    # Test with negative profit
-    # share3 = Share("DEF", 20, -2)
-    # Test with negative price
-    # share3 = Share("DEF", -20, 2)
+    try:
+        share4 = Share("GHI", 0, 2)
+    except ValueError as e:
+        print(e)
